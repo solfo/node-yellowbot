7a408a35b1b3316f55195a4396c54cdcbc8c26ac
Ask Bjørn Hansen <ask@develooper.com>
2013-01-15 12:53:01 -0800

    Be careful not to change the configuration hash

--- test/instances.js
+++ test/instances.js
@@ -1,5 +1,6 @@
 var vows = require('vows'),
     assert = require('assert'),
+    _    = require('underscore'),
     util = require('util');
 
 var ypapi = require('./../lib/yellowbot.js');
@@ -8,7 +9,11 @@ var api1 = ypapi.create()
 var api2 = ypapi.create();
 
 ypapi.configure({api_key: "base", api_secret: "789"});
-api1.configure({api_key: "abc", api_secret: "123"});
+var config = {
+	api_key: "abc", api_secret: "123"
+};
+var config_backup = _.clone(config);
+api1.configure(config);
 api2.configure({api_key: "def", api_secret: "456"});
 
 vows.describe("instances").addBatch({
@@ -16,5 +21,9 @@ vows.describe("instances").addBatch({
         assert.notEqual(api1.server.api_key, api2.server.api_key);
         assert.notEqual(ypapi.server.api_key, api1.server.api_key);
     },
+    "config hash didn't change": function () {
+        assert.deepEqual(config, config_backup);
+    },
+
 }).export(module, {error: false});
 
